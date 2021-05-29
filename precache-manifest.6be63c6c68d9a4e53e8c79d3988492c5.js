70884a32031142d2dc5c426232f2530cbae2d4c4
Siddharth308 <prabhu.sid97@gmail.com>
2021-05-30 04:02:53 +0530

    Updates

---
+++ precache-manifest.6be63c6c68d9a4e53e8c79d3988492c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e33ddee128886f636463ab14a8d3547",
+    "url": "/star_wars/index.html"
+  },
+  {
+    "revision": "4e9cb99aee6d373a8d78",
+    "url": "/star_wars/static/css/main.074f85d4.chunk.css"
+  },
+  {
+    "revision": "1704bede3de0c9fc37c0",
+    "url": "/star_wars/static/js/2.b051db0f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/star_wars/static/js/2.b051db0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e9cb99aee6d373a8d78",
+    "url": "/star_wars/static/js/main.9daea9a1.chunk.js"
+  },
+  {
+    "revision": "97d6669a07f2976518a1",
+    "url": "/star_wars/static/js/runtime-main.029ffd3a.js"
+  },
+  {
+    "revision": "af0d5cf88e51938516e9ef3c91aa4915",
+    "url": "/star_wars/static/media/bg2.af0d5cf8.jpg"
+  }
+]);
